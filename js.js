a2da5757cd6928eb136c40969b54e216284715ed
Batman <alejo.ytuza@gmail.com>
2024-08-11 21:42:01 -0500

    vsincio11dom

---
+++ js.js
@@ -0,0 +1,13 @@
+document.getElementById("app-title")
+//selecciono con el id 
+document.querySelector("#app-title")
+// selecciono el primer elemento y siempre poniendo el n umeral primero #
+document.querySelector("p")
+//trae el primer elemento en este caso primero
+document.getElementsByClassName("menu-items")
+//todos los elmentos que tengan la clase
+document.getElementByTagName("p")
+//todos los tag con ese nombre
+document.querySelectorAll(".menu-items")
+//todos los elementos con ese nodo empieza con .
+//getElement es mas rapido que querySelector
